709442c2af583d91f4163c9a806e34257f9639d8
Corama Yunus <coramayunus@gmail.com>
2021-02-18 20:09:58 +0300

    Download dependencies and set global sequelize

---
+++ utils/database.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.MYSQL_DB,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    dialect: "mysql",
+    host: "localhost",
+  }
+);
+
+const print = async() => {
+    try {
+        await sequelize.authenticate();
+        console.log("Connection successfully");
+      } catch (error) {
+        console.log(error);
+      }
+}
+print();
+module.exports = sequelize;
