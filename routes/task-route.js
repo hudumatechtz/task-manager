700008228c029a54621100afaa3fb3d61b1674a8
Corama Yunus <coramayunus@gmail.com>
2021-02-19 01:04:37 +0300

    Add routes for tasks crud methods

--- routes/task-route.js
+++ routes/task-route.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const taskController = require('../controllers/task');
-const isAuth = require('../middlewares/is-auth')
+const taskController = require("../controllers/task");
+const isAuth = require("../middlewares/is-auth");
+
+router.get("/tasks", isAuth, taskController.getTasks);
+
+router.get("/ongoing/:ongoing", isAuth, taskController.getOnGoing);
+
+router.get("/completed/:completed", isAuth, taskController.getCompleted);
+
+router.get("/new-task/:newtask", isAuth, taskController.getNew);
+
+router.delete("/delete/:taskId", isAuth, taskController.deleteTask);
+
+router.post("/create-task", isAuth, taskController.postTask);
+
+router.get("/edit/:taskId", isAuth, taskController.getTaskForEdit);
+
+router.post("/edit", isAuth, taskController.postEditTask);
 
 module.exports = router;
