9875ac4d73f6c3aa773a01be1701d1fbcef66cab
Corama Yunus <coramayunus@gmail.com>
2021-02-19 01:04:03 +0300

    Populate task controller with CRUD functions

--- controllers/task.js
+++ controllers/task.js
@@ -1,11 +1,12 @@
 const Task = require("../models/task");
 
 exports.getTaskForEdit = async (req, res, next) => {
-  const task = req.params.task;
+  const taskId = req.params.taskId;
   try {
     const task = await Task.findOne({
       where: {
-        task: task,
+        id: task,
+        userId: req.userId,
       },
     });
     if (!task) {
@@ -25,6 +26,7 @@ exports.postEditTask = async (req, res, next) => {
     let task = await Task.findOne({
       where: {
         id: taskId,
+        userId: req.userId,
       },
     });
     if (!task) {
@@ -53,7 +55,8 @@ exports.deleteTask = async (req, res, next) => {
   try {
     const task = await Task.findOne({
       where: {
-        id: req.taskId,
+        id: taskId,
+        userId: req.userId,
       },
     });
     const destroyedTask = await task.destroy();
@@ -63,6 +66,88 @@ exports.deleteTask = async (req, res, next) => {
       throw error;
     }
     res.status(200).json({ message: "TASK DELETED SUCCESSFULLY" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getTasks = async (req, res, next) => {
+  const userId = req.userId;
+  try {
+    const userTasks = Task.findAll({
+      where: {
+        userId: userId,
+      },
+    });
+    if (!userTasks) {
+      const error = new Error("TASKS DO NOT EXIST/ OR COULD NOT FETCHED");
+      error.statusCode = 501;
+      throw error;
+    }
+    res.json({ tasks: userTasks });
+  } catch (error) {
+    next(error);
+  }
+};
+exports.postTask = async (req, res, next) => {
+  const userId = req.userId;
+  const { task } = req.body;
+  try {
+    const createdTask = await Task.create({
+      task: task,
+      userId: userId,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+exports.getOnGoing = async (req, res, next) => {
+  const onGoing = req.params.ongoing;
+  try {
+    const taskOnGoing = await Task.findAll({
+      where: {
+        userId: req.userId,
+        onGoing: true,
+      },
+    });
+    if (!taskOnGoing) {
+      return res.json({ onGoing: null });
+    }
+    res.json({ message: "GOT SOME ONGOING TASKS", onGoing: taskOnGoing });
+  } catch (error) {
+    next(error);
+  }
+};
+exports.getNew = async (req, res, next) => {
+  const newTask = req.params.newtask;
+  try {
+    const taskNew = await Task.findAll({
+      where: {
+        userId: req.userId,
+        newTask: true,
+      },
+    });
+    if (!taskNew) {
+      return res.json({ taskNew: null });
+    }
+    res.json({ message: "GOT SOME NEW TASKS", taskNew: taskNew });
+  } catch (error) {
+    next(error);
+  }
+};
+exports.getCompleted = async (req, res, next) => {
+  const completed = req.params.completed;
+  try {
+    const completed = await Task.findAll({
+      where: {
+        userId: req.userId,
+        completed: true,
+      },
+    });
+    if (!completed) {
+      return res.json({ completed: null });
+    }
+    res.json({ message: "GOT SOME COMPLETED TASKS", completed: completed });
   } catch (error) {
     next(error);
   }
