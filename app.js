8a40743154ae3b048dbfdb0a4d0915cb501fdf10
Corama Yunus <coramayunus@gmail.com>
2021-02-19 01:03:23 +0300

    Add associations for user and tasks

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const sequelize = require("./utils/database");
 require("dotenv").config();
 
@@ -8,10 +7,12 @@ const authRoute = require("./routes/auth-route");
 const userRoute = require("./routes/user-route");
 const taskRoute = require("./routes/task-route");
 
+const User = require("./models/user");
+const Task = require("./models/task");
+
 const PORT = process.env.PORT || 5000;
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.urlencoded({ urlencoded: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "*");
@@ -24,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/account", authRoute);
-// app.use(userRoute);
+app.use(userRoute);
 // app.use(taskRoute);
 
 app.use("/", (req, res, next) => {
@@ -42,10 +43,21 @@ app.use((err, req, res, next) => {
   console.log(err);
   next();
 });
+
+Task.belongsTo(User, {
+  constraints: true,
+  onDelete: "CASCADE",
+});
+User.hasMany(Task);
 sequelize
   .sync()
   .then((result) => {
     // console.log(result);
-    app.listen(PORT);
+    app.listen(PORT, (err) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("TASK MANAGER up and running at PORT:", PORT);
+    });
   })
   .catch((err) => console.log(err));
